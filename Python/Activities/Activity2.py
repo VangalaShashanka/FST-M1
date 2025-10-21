9e0345e2491ba45e26a6c53284a25a4dc2eb3d47
VangalaShashanka <Vangala.Shashanka@ibm.com>
2025-10-21 14:09:39 +0530

    Add files via upload

---
+++ Python/Activities/Activity2.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+age = int (input("Enter your age: "))
+
+# calculate the birth year
+year_when_100 = 2025 + (100-age)
+
+#print the result
+print(name+ " will turn 100 in the year " +str(year_when_100))
